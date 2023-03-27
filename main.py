01950ea4c25b07cffdc2f98533757966372fe454
₸ornike <20404466+e-tornike@users.noreply.github.com>
2023-03-27 11:10:39 +0200

    handle NoneType

--- main.py
+++ main.py
@@ -60,9 +60,10 @@ def main():
             pwc_response = urllib.request.urlopen(client)
             if pwc_response.getcode() == 200:
                 r = json.load(pwc_response)
-                repo_url = r.get("official", {}).get("url", "")
-                if repo_url:
-                    paper['repo_url'] = repo_url
+                if r:
+                    repo_url = r.get("official", {}).get("url", "")
+                    if repo_url:
+                        paper['repo_url'] = repo_url
         except urllib.error.HTTPError:
             print(f'URL not found: {PWC_URL + paper_number}')
 
